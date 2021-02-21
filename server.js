aa11e65d3bfbe5a7dc64cf10f2be8fd46edcfcfd
hamzashamoun96 <hamza.shamoun96@gmail.com>
2021-02-21 15:46:44 +0200

    lab06

---
+++ server.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const express = require('express');
+const server = express();
+
+const dot =  require('dotenv');
+dot.config();
+
+
+const PORT = process.env.Port || 4000;
+
+server.listen(PORT, () => {
+    console.log(`Server is listening on Port ${PORT}`)
+})
+
+server.get('/location',(req , res)=>{
+    const locData = require('./data/location.json')
+    const locObj = new Location (locData);
+    res.send(locObj);
+})
+
+server.get('/weather',(req , res)=>{
+    const wetherData = require('./data/weather.json');
+    let objectArr = [];
+    wetherData.data.forEach(value => {
+        const wetherObj = new Weather (value)
+        objectArr.push(wetherObj)
+    })
+    res.send(objectArr);
+    // res.send(objectArr);
+})
+
+function Location (locData){
+    this.search_query = 'Lynnwood';
+    this.formatted_query = locData[0].display_name;
+    this.latitude = locData[0].lat;
+    this.longitude = locData[0].lon;
+}
+function Weather (wetherData){
+    this.forecast = wetherData.weather.description;
+    this.time = wetherData.valid_date;
+}
