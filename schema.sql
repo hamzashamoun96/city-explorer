2043988378e9373cdfbaa77d817d22fcf0a40bc9
hamzashamoun96 <hamza.shamoun96@gmail.com>
2021-02-23 23:49:06 +0200

    lab08 finish

---
+++ schema.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS locations;
+CREATE TABLE locations (
+  search_query VARCHAR(255),
+  formatted_query VARCHAR(255),
+  latitude NUMERIC(20, 14),
+  longitude NUMERIC(20, 14)
+);
+
+INSERT INTO locations VALUES ('ammman','irbid');
