d4e48ca8b7ed8b8019d66c80908fa020737feb16
hamzashamoun96 <hamza.shamoun96@gmail.com>
2021-02-24 17:29:16 +0200

    finish lab09 and fix lab08

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 'use strict';
 
+
 const express = require('express');
 const server = express();
 const dot = require('dotenv');
 dot.config();
 const pg = require('pg')
 
-// const client = new pg.Client(process.env.DATABASE_URL)
-const client = new pg.Client({ connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false } });
+const client = new pg.Client(process.env.DATABASE_URL)
+// const client = new pg.Client({ connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false } });
 client.connect();
 const cors = require('cors');
 
@@ -15,6 +16,7 @@ server.use(cors());
 
 const port = process.env.PORT || 4000;
 
+const axios = require('axios')
 
 
 const superAgent = require('superagent')
@@ -25,6 +27,8 @@ const superAgent = require('superagent')
 server.get('/location', locHandler)
 server.get('/weather', wetherHandler)
 server.get('/parks', parksHandler)
+server.get('/movies', moviesHandler)
+server.get('/yelp', yelpHandler)
 server.get('*', errHandler)
 
 
@@ -34,43 +38,36 @@ server.get('*', errHandler)
 function locHandler(req, res) {
 
     const cityName = req.query.city
-    let trigger = true;
     let key = process.env.LOCATION_KEY;
     let url = `https://api.locationiq.com/v1/autocomplete.php?key=${key}&q=${cityName}`;
     let SQL = `SELECT * FROM locations;`;
 
     client.query(SQL)
         .then(result => {
-            result.rows.forEach(element => {
-                if (element.search_query == cityName) {
-                    let SQL = `SELECT * FROM locations WHERE search_query='${cityName}';`
-                    // trigger = true;
-                    client.query(SQL)
-                        .then(result => {
-                            let x = new LocationFromDB(cityName, result.rows[0])
-                            res.send(x)
-                            trigger = false
-                        })
-                }
-            });
-            if (trigger) {
+            if (result.rows.filter(value => value.search_query === `${cityName}`).length > 0) {
+                let SQL = `SELECT * FROM locations WHERE search_query='${cityName}';`
+                client.query(SQL)
+                    .then(result => {
+                        let x = new LocationFromDB(cityName, result.rows[0])
+                        res.send(x)
+                    })
+            } else {
                 console.log("Adding To DataBase");
-                superAgent.get(url).then(locData => {
-                    let x = new Location(cityName, locData.body[0])
 
-                    let citynam = x.search_query;
-                    let formN = x.formatted_query;
-                    let lati = x.latitude;
-                    let long = x.longitude;
-                    let sql = `INSERT INTO locations VALUES($1,$2,$3,$4);`
-                    let allRes = [citynam, formN, lati, long]
-                    client.query(sql, allRes)
-                        .then((result) => {
-                            // res.send(result.rows);
-                            // res.send('data has been inserted!!');
+                superAgent.get(url)
+                    .then(result => {
+                        let x = new Location(cityName, result.body[0])
+                        let cityname = x.search_query;
+                        let format = x.formatted_query;
+                        let lati = x.latitude;
+                        let long = x.longitude;
+                        let SQL = `INSERT INTO locations VALUES ($1,$2,$3,$4) RETURNING *;`;
+                        let safeValues = [cityname,format,lati,long];
+                        client.query(SQL,safeValues)
+                        .then(()=>{
+                            res.send(x)
                         })
-                    res.send(x)
-                })
+                    })
             }
         })
 }
@@ -106,6 +103,45 @@ function parksHandler(req, res) {
             res.send(parksObjArr);
         })
 }
+// http://localhost:2000/movies?search_query=london&formatted_query=London%2C%20London%2C%20England%2C%20SW1A%202DX%2C%20United%20Kingdom&latitude=51.50732190000000&longitude=51.50732190000000&page=1
+function moviesHandler(req, res) {
+
+    const cityName = req.query.search_query;
+    let key = process.env.MOVIES_KEY;
+    let url = `https://api.themoviedb.org/3/search/movie?api_key=${key}&query=${cityName}`
+
+    superAgent.get(url)
+        .then(moviesData => {
+            let moviesObjArr = moviesData.body.results.map(value => {
+                return new Movies(value)
+            })
+            res.send(moviesObjArr)
+        })
+}
+// http://localhost:2000/yelp?search_query=seattle&formatted_query=Seattle%2C%20Seattle%2C%20Washington%2C%2098104%2C%20USA&latitude=47.6038321&longitude=-122.3300624&page=1
+function yelpHandler(req, res) {
+    let cityName = req.query.search_query;
+    let page = req.query.page;
+    const limitNum = 5;
+    const start = ((page - 1) * limitNum + 1)
+    let key = process.env.YELP_KEY;
+    let url = `https://api.yelp.com/v3/businesses/search?location=${cityName}&limit=${limitNum}&offset=${start}`
+
+    let yelpRest = axios.create({
+        baseURL: "https://api.yelp.com/v3/",
+        headers: {
+            Authorization: `Bearer ${key}`,
+            "Content-type": "application/json",
+        },
+    })
+    yelpRest(url)
+        .then(({ data }) => {
+            let yelpObjArr = data.businesses.map(value => {
+                return new Yelp(value)
+            })
+            res.send(yelpObjArr)
+        })
+}
 
 function errHandler(req, res) {
     let errObj = {
@@ -114,7 +150,6 @@ function errHandler(req, res) {
     }
     res.send(errObj);
 }
-
 
 // Constructors
 
@@ -143,7 +178,21 @@ function Parks(parksData) {
     this.description = parksData.description;
     this.url = parksData.url;
 }
-
+function Movies(movieData) {
+    this.title = movieData.title;
+    this.overview = movieData.overview;
+    this.average_votes = movieData.vote_average;
+    this.image_url = `https://image.tmdb.org/t/p/w500${movieData.poster_path}`;
+    this.popularity = movieData.popularity;
+    this.released_on = movieData.release_date;
+}
+function Yelp(yelpData) {
+    this.name = yelpData.name;
+    this.image_url = yelpData.image_url;
+    this.price = yelpData.price;
+    this.rating = yelpData.rating;
+    this.url = yelpData.url;
+}
 server.listen(port, () => {
     console.log(`Server is listening on Port ${port}`)
 })
