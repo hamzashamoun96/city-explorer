56db89988f9d8e3094d12788018b1939201d57e2
hamzashamoun96 <hamza.shamoun96@gmail.com>
2021-02-24 17:30:22 +0200

    finish lab09 and fix lab08

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const dot = require('dotenv');
 dot.config();
 const pg = require('pg')
 
-const client = new pg.Client(process.env.DATABASE_URL)
-// const client = new pg.Client({ connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false } });
+// const client = new pg.Client(process.env.DATABASE_URL)
+const client = new pg.Client({ connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false } });
 client.connect();
 const cors = require('cors');
 
